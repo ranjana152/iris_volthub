e8c2d32ad77687abb153040e837d0c5c651bfdbd
ranjana152 <ranjana.ravindran22@gmail.com>
2025-03-21 17:33:25 +0530

    Drizzle set up done

--- backend_volthub/src/app.ts
+++ backend_volthub/src/app.ts
@@ -1,11 +1,25 @@
 import express from 'express';
-const app = express();
-const port = 3000;
+import dotenv from 'dotenv';
+import { drizzle } from 'drizzle-orm/node-postgres';
 
+dotenv.config();
+const app = express();
+const port = process.env.PORT || 3000;
+const db = drizzle(process.env.DATABASE_URL!);
+
+
+app.get("/check-db", async (req, res) => {
+    try {
+        res.json({ success: true, message: "Database connection is successful" });
+    } catch (error) {
+        console.error("Database connection error:", error);
+        res.status(500).json({ success: false, message: "Database connection failed", error: error.message });
+    }
+});
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+    res.send('Hello World!');
 });
 
 app.listen(port, () => {
-  return console.log(`Express is listening at http://localhost:${port}`);
+    return console.log(`Express is listening at http://localhost:${port}`);
 });
